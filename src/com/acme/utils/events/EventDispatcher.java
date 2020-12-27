fff201058bd25c5cd75170371b9357d6a9a410b0
acloudfan <raj@acloudfan.com>
2020-12-26 19:17:54 -0500

    events are good way

---
+++ src/com/acme/utils/events/EventDispatcher.java
@@ -0,0 +1,47 @@
+package com.acme.sales.model.utils.events;
+
+import java.util.ArrayList;
+import com.acme.sales.model.utils.events.EventHandler;
+
+
+/**
+ * Handles the execution of the Handler
+ *
+ * Each dispatcher will handle only ONE type of event
+ */
+public class EventDispatcher {
+
+    public final String eventName;
+
+    ArrayList<EventHandler>  handlers = new ArrayList<>();
+
+    public EventDispatcher(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public boolean register(EventHandler handler){
+        // Make sure that Hanlder is not registered already - otherwise you have duplication issues !!!
+        if(handlers.contains(handler)){
+            return false;
+        }
+
+        return handlers.add(handler);
+    }
+
+    public boolean deregister(EventHandler handler){
+        return handlers.remove(handler);
+    }
+
+    /**
+     * Executes the event handler code for all registered handlers
+     * @param event
+     */
+    public boolean dispatch(Event event){
+        if(handlers.size() == 0) return false;
+
+        for(EventHandler handler : handlers){
+            handler.handle(event);
+        };
+        return true;
+    }
+}
---
+++ src/com/acme/utils/events/EventHandler.java
@@ -0,0 +1,10 @@
+package com.acme.sales.model.utils.events;
+
+/**
+ * Handles the events
+ */
+public interface EventHandler {
+    public void handle(Event  event);
+}
+
+
